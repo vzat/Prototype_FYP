eae5513e049db88f424653e5b638e2fafc57cc15
Vlad Zat <vlad.zat@mydit.ie>
2017-11-21 13:05:42 +0000

    Add Connection Manager Cleint

--- connection-manager/client/src/App.js
+++ connection-manager/client/src/App.js
@@ -4,7 +4,18 @@ import './App.css';
 
 class App extends Component {
   state = {
-      conns: ""
+      connections: {
+          name: '',
+          address: '',
+          port: '',
+          dbuser: '',
+          dbpass: ''
+      },
+      account: {
+          username: '',
+          email: '',
+          token: ''
+      }
   };
 
   componentDidMount = () => {
@@ -12,34 +23,83 @@ class App extends Component {
   };
 
   getConnections = () => {
-    db.getConnections('jsmith', connections => {
-      this.setState({
-        conns: connections
-      })
+    const username = 'jsmith';
+    db.getConnections(username, connections => {
+          for (const connNo in connections) {
+              const connection = connections[connNo];
+
+              if (connection.username === username) {
+                  // Set Account Info
+                  const account = {
+                      username: connection.username,
+                      email: connection.email,
+                      token: connection.token
+                  };
+                  this.setState({
+                      account: account
+                  });
+
+                  // Set Databases Info
+                  if (connection.databases) {
+                      let conns = [];
+                      for (const databaseNo in connection.databases) {
+                          const database = connection.databases[databaseNo];
+                          const conn = {
+                              name: database.name,
+                              address: database.address,
+                              port: database.port,
+                              dbuser: database.username,
+                              dbpass: database.password
+                          }
+                          conns.push(conn);
+                      }
+                      if (conns.length === 0) {
+                          const conn = {
+                              empty: 1
+                          }
+                          conns.push(conn);
+                      }
+                      this.setState({
+                          connections: conns
+                      });
+                  }
+              }
+          }
     });
   };
 
   render() {
-    let connections = this.state.conns;
+    let connections = this.state.connections;
     let connectionList = Object.keys(connections).map((connectionNo) => (
         <div className="connection">
-            Database: {connections[connectionNo]}
+            Database: {connections[connectionNo].name}
+            <br/>
+            URI:
+            mongodb://
+            {connections[connectionNo].dbuser}:{connections[connectionNo].dbpass}
+            @{connections[connectionNo].address}:{connections[connectionNo].port}
+            /{connections[connectionNo].name}
         </div>
-
-        // <li key={connectionNo}>
-        //     {connections[connectionNo]}
-        //     <button>View</button>
-        // </li>
     ));
+    let account = this.state.account;
 
     return (
       <div className="App">
         <header className="App-header">
           <h1> Connection Manager </h1>
         </header>
-        <ul>
-          {connectionList}
-        </ul>
+
+        <div class="account">
+          Username: {account.username}
+          <br/>
+          Email: {account.email}
+          <br/>
+          API Token: {account.token}
+          <br/>
+        </div>
+
+        {connectionList}
+        <br/>
         <button>Create Connection</button>
       </div>
     );
--- connection-manager/client/src/utils/db.js
+++ connection-manager/client/src/utils/db.js
@@ -6,23 +6,24 @@ function getConnections(username, connections) {
             'Authorization': 'Bearer z321'
         }
     }).then(function (res) {
-        return res.json().then(function (json) {
-            for (const recordNo in json) {
-                const record = json[recordNo];
-
-                if (record.username === username && record.databases) {
-                    let conns = [];
-                    for (const databaseNo in record.databases) {
-                        conns.push(record.databases[databaseNo].name);
-                    }
-                    if (conns.length === 0) {
-                        conns.push('You have no connections');
-                    }
-                    return conns;
-                }
-            }
-            return ['Username not found'];
-        })
+        return res.json();
+        // .then(function (json) {
+        //     for (const recordNo in json) {
+        //         const record = json[recordNo];
+        //
+        //         if (record.username === username && record.databases) {
+        //             let conns = [];
+        //             for (const databaseNo in record.databases) {
+        //                 conns.push(record.databases[databaseNo].name);
+        //             }
+        //             if (conns.length === 0) {
+        //                 conns.push('You have no connections');
+        //             }
+        //             return conns;
+        //         }
+        //     }
+        //     return ['Username not found'];
+        // })
     }).then(connections);
 }
 
