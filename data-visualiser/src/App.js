fb1488db24ff6884f950cff52d8033bd890a09ee
Vlad Zat <vlad.zat@mydit.ie>
2017-11-26 00:34:18 +0000

    Add side menu and content

--- data-visualiser/src/App.js
+++ data-visualiser/src/App.js
@@ -1,19 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 class App extends Component {
   render() {
+    const styles = {
+        paper: {
+            height: "100%",
+            width: "100%"
+        },
+        menu: {
+            width: "200px"
+        },
+        tabs: {
+            width: "100%"
+        }
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+          <div className="App">
+
+              <div className = "Menu">
+                  <Paper style = {styles.paper}>
+                      <Menu style = {styles.menu}>
+                          <MenuItem primaryText="DB1" />
+                          <MenuItem primaryText="DB2" />
+                      </Menu>
+                  </Paper>
+              </div>
+
+              <div className = "Content">
+                  <Tabs style = {styles.tabs}>
+                      <Tab label = "Raw Data">
+                          <div className = "RawData">
+                              Raw Data
+                          </div>
+                      </Tab>
+                      <Tab label = "Visualised Data">
+                          <div className = "PrettyData">
+                              Pretty Data
+                          </div>
+                      </Tab>
+                  </Tabs>
+              </div>
+
+          </div>
+      </MuiThemeProvider>
     );
   }
 }
